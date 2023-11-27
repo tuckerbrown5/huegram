538afff79da040051c6f3a241ecc002f9e8d191a
Kenan Casey <kenancasey@gmail.com>
2023-11-27 14:24:23 -0600

    Fixes from Monday

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 function App() {
 
   const [hues, setHues] = useState([
-    {id: 1, color: '#ffa500', username: "kaylee", likes: 15},
+    {id: 1, color: '#ffa500', username: "kaylee", likes: 15, isLiked:false},
     {id: 2, color: '#ff8c00', username: "therealkaylee", likes: 8},
     {id: 3, color: '#ff7f50', username: "odomester", likes: 20},
     {id: 4, color: '#ff6347', username: "kaylee", likes: 200},
@@ -14,18 +14,24 @@ function App() {
     {id: 6, color: '#ff6747', username: "abbieV", likes: 13},
   ]);
 
+  const [currentUser, setCurrentUser] = useState({
+    username: "kavery",
+    likes: 58,
+    hues: [ {id:36, color:'#ffa510', username:"kavery", likes: 15}]
+  });
+
   const addNewHue = (color:string ) => 
   {
       console.log(color)
-      const newHue = {color, username: "kaylee", id: hues[hues.length-1].id+1 , likes:0};
+      const newHue = {color, username: currentUser.username, id: hues[hues.length-1].id+1 , likes:0};
       setHues( [...hues, newHue ] );
   }
 
   return (
     <div className='flex bg-slate-800 h-screen'>
-      <Menu />
+      {/* <Menu /> */}
 
-      <Main hues={hues} addHue= {addNewHue} />
+      <Main hues={hues} addHue = {addNewHue} />
 
       <Profile />
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Post from './Hue'
+import Hue from './Hue'
 import PostHue from './PostHue'
 
 interface HueObject {
