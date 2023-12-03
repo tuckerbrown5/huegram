b0ec0cefc09871f03d76049c0b343582c1e1f926
tuckerbrown5 <tucker.brown@students.fhu.edu>
2023-12-02 23:26:28 -0600

    more

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,9 @@
 import Hue from './Hue'
 import PostHue from './PostHue'
+import Title from './Title'
 
 interface HueObject {
-    color: string,
+    hex_code: string,
     username: string,
     likes: number
 }
@@ -11,22 +12,26 @@ interface Props {
     addHue: (color:string) => void
 }
 
-const Main = (props : Props) => {
+const Main = ({hues, addHue} : Props) => {
   return (
-    <div className='flex flex-wrap w-full justify-center gap-8 overflow-y-auto'>
+    <div className='flex flex-col flex-grow bg-slate-800'>
+      <Title></Title>
+      <div className="flex flex-wrap w-full justify-center gap-8 overflow-y-auto m-5">
+          <div className='grid grid-cols-4 gap-8 overflow-y-auto'>
+      
 
-        <PostHue addHue={props.addHue}/>
+        <PostHue addHue={addHue}/>
 
 
-        {props.hues.map(  (hue) => ( 
+        {hues.map( (hue) => ( 
             
             <Hue hue={hue}/>
-       ))}
+        ))}
 
-        
-
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Main
+export default Main;
--- src/components/PostHue.tsx
+++ src/components/PostHue.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 interface Props {
   addHue: (hex_code: string) => void;
